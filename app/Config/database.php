2d7e7ec9859e162576ab955307016c74364c721e
WPS PS <petr.svacha@webpagesoftware.co.uk>
2022-08-10 08:16:21 +0200

    Database - set default engine to "InnoDB" (regarding "Specified key was too long, max key..." migration error)

--- app/Config/database.php
+++ app/Config/database.php
@@ -75,7 +75,7 @@ return [
             'prefix'         => env('DB_TABLE_PREFIX', ''),
             'prefix_indexes' => true,
             'strict'         => false,
-            'engine'         => null,
+            'engine'         => 'InnoDB',
             'options'        => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
