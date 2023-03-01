52b7c939451451212e948c7fb4758429ebccb319
vacwel <vaclav@webpagesoftware.co.uk>
2023-03-01 15:33:48 +0100

    amends

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -46,9 +46,9 @@
 
                 @if(!signedInUser())
                     @if(setting('registration-enabled') && config('auth.method') === 'standard')
-                        <a href="{{ url('/register') }}">@icon('new-user'){{ trans('auth.sign_up') }}</a>
+                        <!--a href="{{ url('/register') }}">@icon('new-user'){{ trans('auth.sign_up') }}</a-->
                     @endif
-                    <a href="{{ url('/login')  }}">@icon('login'){{ trans('auth.log_in') }}</a>
+                    <!--a href="{{ url('/login')  }}">@icon('login'){{ trans('auth.log_in') }}</a-->
                 @endif
             </div>
             @if(signedInUser())
